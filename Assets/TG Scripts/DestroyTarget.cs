b20e344c3d1b636871d00586ecc04aba44bf6f8f
Thomas Goodge <2605440g@student.gla.ac.uk>
2022-08-15 18:51:29 +0100

    Clean up of extraneous files

--- Assets/TG Scripts/DestroyTarget.cs
+++ Assets/TG Scripts/DestroyTarget.cs
@@ -6,7 +6,13 @@ public class DestroyTarget : MonoBehaviour
 {
     public void DestroyThisTarget()
     {
-        //Method that destroys the current game object. Used as a method called when object is focused on in the Hololens
+
+             //Method that destroys the current game object. Used as a method called when object is focused on in the Hololens
         Destroy(this.gameObject);
+
+        
+   
+
+
     }
 }
